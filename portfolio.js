6e5472cb64bb4d5f3fbde6a6347bc849d46c7466
Hasan Pourghannad <mhpourghannad@gmail.com>
2025-03-16 15:32:54 +0330

    add portfolio.js file

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+const portfolio = [
+    {
+        name: '90theme-version-1',
+        type: 'website',
+        design: 'Pourghannad',
+        development: 'Pourghannad',
+        platform: 'WordPress',
+        description: '',
+        year: '2013',
+    }
+]

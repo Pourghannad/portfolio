ef734e9c0cfc0b63fcd628eec9b0144a0f611387
Hasan Pourghannad <mhpourghannad@gmail.com>
2025-10-18 16:57:35 +0330

    fixed type issue in canvasRef

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,31 +1,36 @@
 import { useEffect, useRef } from "react";
 import style from "./list.module.scss";
-import dopDots from "@pourghannad/dopdots"
+import dopDots from "@pourghannad/dopdots";
 
 export default function List() {
-  const canvasRef = useRef(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     if (canvasRef.current) {
-      dopDots(canvasRef.current.getContext("2d"))
+      const ctx = canvasRef.current.getContext("2d");
+      if (ctx) {
+        dopDots(ctx);
+      }
     }
   }, []);
   return (
     <div className={style["list-container"]}>
-      <canvas
-        ref={canvasRef}
-        id="dopdots"
-        width="400px"
-        height="400px"
-        data-margin="10"
-        data-type="grid"
-        data-elementsize="6"
-        data-color="#3c3c3c"
-        data-elementrect="true"
-        data-secondrect="true"
-        data-elementformousemove=".dopdots-wrapper"
-        data-moveradius="300"
-        data-secondColor="#0c130b"
-      ></canvas>
+      <div className="dopdots-wrapper">
+        <canvas
+          ref={canvasRef}
+          id="dopdots"
+          width="400px"
+          height="400px"
+          data-margin="10"
+          data-type="grid"
+          data-elementsize="6"
+          data-color="#3c3c3c"
+          data-elementrect="true"
+          data-secondrect="true"
+          data-elementformousemove=".dopdots-wrapper"
+          data-moveradius="300"
+          data-secondColor="#0c130b"
+        ></canvas>
+      </div>
     </div>
   );
 }

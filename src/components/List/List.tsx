9ff1f6705b6ee1c91cc16603e198f59975cdc420
Hasan Pourghannad <mhpourghannad@gmail.com>
2025-10-18 16:34:41 +0330

    init dopdots

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useRef } from "react";
 import style from "./list.module.scss";
+import dopDots from "@pourghannad/dopdots"
 
 export default function List() {
-  return <div className={style["list-container"]}>List</div>;
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    if (canvasRef.current) {
+      dopDots(canvasRef.current.getContext("2d"))
+    }
+  }, []);
+  return (
+    <div className={style["list-container"]}>
+      <canvas
+        ref={canvasRef}
+        id="dopdots"
+        width="400px"
+        height="400px"
+        data-margin="10"
+        data-type="grid"
+        data-elementsize="6"
+        data-color="#3c3c3c"
+        data-elementrect="true"
+        data-secondrect="true"
+        data-elementformousemove=".dopdots-wrapper"
+        data-moveradius="300"
+        data-secondColor="#0c130b"
+      ></canvas>
+    </div>
+  );
 }

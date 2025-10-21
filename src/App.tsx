6588d9c489e7759c9cb701afaefcbc20dbd30d74
Hasan Pourghannad <mhpourghannad@gmail.com>
2025-10-21 15:06:54 +0330

    temporary add three component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Footer from "./components/Footer/Footer";
+import Three from "./components/Three/Three";
 
 function App() {
   return (
     <main>
+      <Three />
       <Header />
       <List />
       <Footer />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import style from "./header.module.scss";
 
 export default function Header() {
-  return <header className={style["header"]}>MohammadHasan Pourghannad</header>;
+  return <header className={style["header"]}><h1>MohammadHasan Pourghannad</h1></header>;
 }

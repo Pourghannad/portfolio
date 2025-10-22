1c17785ef5639725dc1b08f9236c754a7b9735a5
Hasan Pourghannad <mhpourghannad@gmail.com>
2025-10-22 16:32:22 +0330

    add basic animation for title

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import style from "./header.module.scss";
 
 export default function Header() {
-  return <header className={style["header"]}><h1>MohammadHasan Pourghannad</h1></header>;
+  return <header className={style["header"]}><h1>MohammadHasan Pourghannad<span></span></h1></header>;
 }

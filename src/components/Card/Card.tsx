4a3b2828254f6081d496df29ac4b29a6fd5bed0a
Hasan Pourghannad <mhpourghannad@gmail.com>
2025-11-20 18:35:37 +0330

    add list animation

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,26 +6,27 @@ import PenIcon from "@/icons/pen.svg?react";
 import TechIcon from "@/icons/tech.svg?react";
 import { useCallback, useState } from "react";
 
-const PLACEHOLDER_SRC = `data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D`
+const PLACEHOLDER_SRC = `data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs%3D`;
 
 export default function Card(props: ICardProps) {
   const { data, title, inView } = props;
-    const [hasLoaded, setHasLoaded] = useState(false)
-    const setLoaded = useCallback(() => {
-    if (inView) setHasLoaded(true)
-  }, [inView, setHasLoaded])
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const setLoaded = useCallback(() => {
+    if (inView) setHasLoaded(true);
+  }, [inView, setHasLoaded]);
   return (
     <div className={style["card"]}>
-      <h4>{data.name} {data.description && <span>{data.description}</span>} </h4>
+      <h4>
+        {data.name} {data.description && <span>{data.description}</span>}{" "}
+      </h4>
       {title === "all" && <span className={style["type"]}>{data.type}</span>}
       <div className={style["description"]}>
-        {data.technology && data.technology.length > 0 && 
+        {data.technology && data.technology.length > 0 && (
           <div className={style["tech"]}>
             <TechIcon />
-            {data.technology.join(', ')}
+            {data.technology.join(", ")}
           </div>
-        
-        }
+        )}
       </div>
       <div className={style["badges"]}>
         <span className={style["createdat"]}>{data.createdAt}</span>
@@ -39,9 +40,7 @@ export default function Card(props: ICardProps) {
           <div className={style["design"]}>
             <PenIcon />
             {data.design}
-            <span>
-              Designed by:
-            </span>
+            <span>Designed by:</span>
           </div>
         )}
         {data.link && (
@@ -50,10 +49,16 @@ export default function Card(props: ICardProps) {
           </a>
         )}
       </div>
-      {/* {!hasLoaded && <span className={} />} */}
+      {!hasLoaded && <span className={style["image-loading"]} />}
       {<span className={style["image-loading"]} />}
       {data.images && data.images.length > 0 && (
-        <img data-src={data.images[0]} alt={data.name} src={inView ? data.images[0] : PLACEHOLDER_SRC} onLoad={setLoaded}  className={style["cover"]} />
+        <img
+          data-src={data.images[0]}
+          alt={data.name}
+          src={inView ? data.images[0] : PLACEHOLDER_SRC}
+          onLoad={setLoaded}
+          className={style["cover"]}
+        />
       )}
       {/* {data.images &&
         data.images?.length > 0 &&
